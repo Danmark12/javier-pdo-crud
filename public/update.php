1575ccffa20009ebd3ff99f3a991ef713ae2e15a
Danmark12 <danmarkpetalcurin@gmail.com>
2024-05-13 09:58:12 +0800

    added tbl_dan.sql file

--- public/update.php
+++ public/update.php
@@ -1,48 +1,49 @@
 <?php
 // Include config file
-require_once "config.php";
- 
+require_once "../db/config.php";
+
 // Define variables and initialize with empty values
 $product_id = $product_thumbnail_link = $product_name = $product_description = $product_retail_price = $product_date_added = $product_updated_date = "";
 $Pname_err = $Pdescription_err = $Pprice_err = "";
- 
+
 // Processing form data when form is submitted
-if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
-    // Get hidden input value
-    $id = $_POST["product_id"];
+   
+    if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
+        // Get hidden input value
+        $product_id = $_POST["product_id"];
     
-     // Validate name
-     $input_product_name = trim($_POST["product_name"]);
-     if (empty($input_product_name)) {
-         $Pname_err = "Please enter a product name.";
-     } elseif (!preg_match("/^[a-zA-Z\s]+$/", $input_product_name)) {
-         $Pname_err = "Please enter a valid name.";
-     } else {
-         $product_name = $input_product_name;
-     }
-     
-     // Validate description
-     $input_product_description = trim($_POST["product_description"]);
-     if (empty($input_product_description)) {
-         $Pdescription_err = "Please enter a product description.";     
-     } else {
-         $product_description = $input_product_description;
-     }
-     
-     // Validate retail_price
-     $input_product_retail_price = trim($_POST["product_retail_price"]);
-     if (empty($input_product_retail_price)) {
-         $Pprice_err = "Please enter the retail price amount.";     
-     } elseif (!ctype_digit($input_product_retail_price)) {
-         $Pprice_err = "Please enter a positive integer value.";
-     } else {
-         $product_retail_price = $input_product_retail_price;
-     }
+    // Validate name
+    $input_product_name = trim($_POST["product_name"]);
+    if (empty($input_product_name)) {
+        $Pname_err = "Please enter a product name.";
+    } elseif (!preg_match("/^[a-zA-Z\s]+$/", $input_product_name)) {
+        $Pname_err = "Please enter a valid name.";
+    } else {
+        $product_name = $input_product_name;
+    }
+    
+    // Validate description
+    $input_product_description = trim($_POST["product_description"]);
+    if (empty($input_product_description)) {
+        $Pdescription_err = "Please enter a product description.";     
+    } else {
+        $product_description = $input_product_description;
+    }
+    
+    // Validate retail_price
+    $input_product_retail_price = trim($_POST["product_retail_price"]);
+    if (empty($input_product_retail_price)) {
+        $Pprice_err = "Please enter the retail price amount.";     
+    } elseif (!ctype_digit($input_product_retail_price)) {
+        $Pprice_err = "Please enter a positive integer value.";
+    } else {
+        $product_retail_price = $input_product_retail_price;
+    }
     
     // Check input errors before inserting in database
     if (empty($Pname_err) && empty($Pdescription_err) && empty($Pprice_err)) {
-        // Prepare an update statement
-        $sql = "UPDATE products 
+       // Prepare an update statement
+$sql = "UPDATE products 
 SET product_thumbnail_link=:product_thumbnail_link,
     product_name=:product_name,
     product_description=:product_description,
@@ -51,36 +52,34 @@ SET product_thumbnail_link=:product_thumbnail_link,
     product_updated_date=:product_updated_date
 WHERE product_id=:product_id";
 
-        if($stmt = $pdo->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":product_id", $param_product_id);
-            $stmt->bindParam(":product_thumbnail_link", $param_product_thumbnail_link);
-            $stmt->bindParam(":product_name", $param_product_name);
-            $stmt->bindParam(":product_description", $param_product_description);
-            $stmt->bindParam(":product_retail_price", $param_product_retail_price);
-            $stmt->bindParam(":product_date_added", $param_product_date_added);
-            $stmt->bindParam(":product_updated_date", $param_product_updated_date);
-            
-            
-            // Set parameters
-            $param_product_id = $product_id;
-            $param_product_thumbnail_link = $product_thumbnail_link;
-            $param_product_name = $product_name;
-            $param_product_description = $product_description;
-            $param_product_retail_price = $product_retail_price;
-            $param_product_date_added = $product_date_added;
-            $param_product_updated_date = $product_updated_date;            
-            
-            
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                // Records updated successfully. Redirect to landing page
-                header("location: index.php");
-                exit();
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
+if ($stmt = $pdo->prepare($sql)) {
+// Bind variables to the prepared statement as parameters
+$stmt->bindParam(":product_id", $product_id);
+$stmt->bindParam(":product_thumbnail_link", $product_thumbnail_link);
+$stmt->bindParam(":product_name", $product_name);
+$stmt->bindParam(":product_description", $product_description);
+$stmt->bindParam(":product_retail_price", $product_retail_price);
+$stmt->bindParam(":product_date_added", $product_date_added);
+$stmt->bindParam(":product_updated_date", $product_updated_date);
+
+// Set parameters
+$param_product_id = $product_id;
+$param_product_thumbnail_link = $product_thumbnail_link;
+$param_product_name = $product_name;
+$param_product_description = $product_description;
+$param_product_retail_price = $product_retail_price;
+$param_product_date_added = $product_date_added;
+$param_product_updated_date = $product_updated_date;   
+// Attempt to execute the prepared statement
+if ($stmt->execute()) {
+// Records updated successfully. Redirect to landing page
+header("location: public/welcome.php");
+exit();
+} else {
+echo "Oops! Something went wrong. Please try again later.";
+}
+}
+
          
         // Close statement
         unset($stmt);
@@ -88,30 +87,27 @@ WHERE product_id=:product_id";
     
     // Close connection
     unset($pdo);
-} else{
+} else {
     // Check existence of id parameter before processing further
-    if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    if (isset($_GET["product_id"]) && !empty(trim($_GET["product_id"]))) {
         // Get URL parameter
-        $id =  trim($_GET["id"]);
+        $product_id = trim($_GET["product_id"]);
         
         // Prepare a select statement
-        $sql = "SELECT * FROM employees WHERE product_id = :product_id";
-        if($stmt = $pdo->prepare($sql)){
+        $sql = "SELECT * FROM products WHERE product_id = :product_id";
+        
+        if ($stmt = $pdo->prepare($sql)) {
             // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":product_id", $param_id);
-            
-            // Set parameters
-            $param_id = $id;
-            
+            $stmt->bindParam(":product_id", $product_id, PDO::PARAM_INT);
+            $param_product_id =  $product_id ;
+        
             // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                if($stmt->rowCount() == 1){
-                    /* Fetch result row as an associative array. Since the result set
-                    contains only one row, we don't need to use while loop */
+            if ($stmt->execute()) {
+                if ($stmt->rowCount() == 1) {
+                    // Fetch result row as an associative array
                     $row = $stmt->fetch(PDO::FETCH_ASSOC);
                 
                     // Retrieve individual field value
-                
                     $product_thumbnail_link = $row["product_thumbnail_link"];
                     $product_name = $row["product_name"];
                     $product_description = $row["product_description"];
@@ -119,13 +115,13 @@ WHERE product_id=:product_id";
                     $product_date_added = $row["product_date_added"];
                     $product_updated_date = $row["product_updated_date"];
 
-                } else{
+                } else {
                     // URL doesn't contain valid id. Redirect to error page
-                    header("location: error.php");
+                    header("location:public/error.php");
                     exit();
                 }
                 
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
@@ -135,9 +131,9 @@ WHERE product_id=:product_id";
         
         // Close connection
         unset($pdo);
-    }  else{
+    } else {
         // URL doesn't contain id parameter. Redirect to error page
-        header("location: error.php");
+        header("location: public/error.php");
         exit();
     }
 }
@@ -156,20 +152,20 @@ WHERE product_id=:product_id";
         }
     </style>
 </head>
-<body>
-    <div class="wrapper">
+<div class="wrapper">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <h2 class="mt-5">Update Record</h2>
-                    <p>Please edit the input values and submit to update the employee record.</p>
-                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+                    <h2 class="mt-5">Create Record</h2>
+                    <p>Please fill this form and submit to add a product record to the database.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="form-group">
                         <label>Product ID</label>
                             <input type="text" name="product_id" class="form-control <?php echo (!empty($Pid_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $product_id; ?>">
                             <span class="invalid-feedback"><?php echo $Pid_err; ?></span>
                         </div>
                         <div class="form-group">
+                    
                             <label>Product Thumbnail Link</label>
                             <input type="text" name= "product_thumbnail_link" class="form-control <?php echo (!empty($Plink_err)) ? 'is-invalid' : ''; ?>" value="<?php echo  $product_thumbnail_link?>">
                             <span class="invalid-feedback"><?php echo $Plink_err; ?></span>
@@ -199,7 +195,7 @@ WHERE product_id=:product_id";
                             <input type="date" name="product_updated_date" class="form-control <?php echo (!empty($Pupdated_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $product_updated_date; ?>">
                             <span class="invalid-feedback"><?php echo $Pupdated_err; ?></span>
                         </div>
-                        <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                         <input type="hidden" name="id" value="<?php echo $id; ?>"/>
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <a href="../index.php" class="btn btn-secondary ml-2">Cancel</a>
                     </form>
